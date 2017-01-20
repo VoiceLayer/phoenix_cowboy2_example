f63a92a4d164f78c506729eb62ebe2a1906280d3
Gary Rennie <gazler@gmail.com>
2017-01-20 00:21:04 +0000

    feat(PhoenixCowboy2Example.PageController): show if using HTTP/2

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,12 @@ defmodule PhoenixCowboy2Example.PageController do
   use PhoenixCowboy2Example.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    http2 =
+      case conn.adapter do
+        {_, %{version: :"HTTP/2"}} -> true
+        _                          -> false
+      end
+
+    render conn, "index.html", http2: http2
   end
 end
